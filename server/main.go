8e6cf6ca0c2daddd66835ad976d2b8763c06ae93
capucinoxx <bendertermebc@gmail.com>
2024-09-21 00:25:55 -0400

    wip

--- server/main.go
+++ server/main.go
@@ -1,11 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func setupRoutes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir("../static")))
+
+	return mux
+}
 
 func main() {
-	fs := http.FileServer(http.Dir("../static"))
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: setupRoutes(),
+	}
 
-	http.Handle("/", fs)
+	go func() {
+		server.ListenAndServe()
+	}()
+	fmt.Println("Server started on port 8080")
 
-	http.ListenAndServe(":8080", nil)
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, os.Kill)
+
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	server.Shutdown(ctx)
 }
